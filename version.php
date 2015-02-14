59dbdffa9dc084697a1a85f9ef29a2ff5eea2527
papillon326 <root@webculture.yamaguchi-pu.ac.jp>
2015-02-14 18:02:25 +0900

    update version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015021000;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2015021400;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014111000;        // Requires this Moodle version.
 $plugin->component = 'atto_fontsize';   // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
