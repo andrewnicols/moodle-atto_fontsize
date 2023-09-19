7e3f3d88d9e3a830489fc4395d428631204d5f8a
Andrew Nicols <andrew@nicols.co.uk>
2023-09-19 22:11:43 +0800

    Add privacy API implementation

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -14,18 +14,23 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+namespace atto_fontsize\privacy;
+
 /**
- * Atto text editor integration version file.
+ * Privacy Subsystem for atto_fontsize implementing null_provider.
  *
- * @package    atto_fontsize
- * @copyright  2014 Andrew Nicols 2015 Adam Jenkins
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package atto_fontsize
+ * @copyright 2018 Andrew Nicols <andrew@nicols.co.uk>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->version   = 2023091900;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2014050800;        // Requires this Moodle version.
-$plugin->component = 'atto_fontsize';   // Full name of the plugin (used for diagnostics).
-$plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.8 (Build 2015042701)';
+class provider implements \core_privacy\local\metadata\null_provider {
+    /**
+     * Get the language string identifier with the component's language
+     * file to explain why this plugin stores no data.
+     *
+     * @return  string
+     */
+    public static function get_reason(): string {
+        return 'privacy:metadata';
+    }
+}
